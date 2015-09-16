d29c88ce2e9d8d99496b91da0cec9848ac1289c7
elmo911 <jacobmwolfe@gmail.com>
2015-09-16 17:04:10 -0500

    do while loop

--- loopexample.php
+++ loopexample.php
@@ -2,10 +2,17 @@
 <body>
     <?php
         $count = 0;
-        while ($count < 10){
+        /*while ($count < 10){
             echo "<p>my number in the loop is: ".$count."<br></br>";
             $count++;
         }
+        */
+        
+        do {
+            echo "<p>my number in the loop is: ".$count."<br></br>";
+            $count++;
+        }
+        while($count <= 10);
     ?>
 </body>
 </html>
