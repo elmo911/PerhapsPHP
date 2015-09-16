9a90cc15426f4c3d9c6115e8e948b956c3f4e28f
elmo911 <jacobmwolfe@gmail.com>
2015-09-16 17:08:34 -0500

    for loop

--- loopexample.php
+++ loopexample.php
@@ -8,11 +8,15 @@
         }
         */
         
-        do {
+        /*do {
             echo "<p>my number in the loop is: ".$count."<br></br>";
             $count++;
         }
-        while($count <= 10);
+        while($count <= 10);*/
+        
+        for($newcount = 0; $newcount < 10; $newcount++){
+            echo "<p>my number in the loop is: ".$newcount."<br></br>";
+        }
     ?>
 </body>
 </html>
