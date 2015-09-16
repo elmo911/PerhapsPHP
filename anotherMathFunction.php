0ce4f1a51345e3104ed47a5ef18dc77ddda3894a
elmo911 <jacobmwolfe@gmail.com>
2015-09-16 16:56:28 -0500

    9/16
    Class example

---
+++ anotherMathFunction.php
@@ -0,0 +1,6 @@
+<?php
+Class MathFunctions {
+    public $function;
+    
+    public function addthem ($num1, $num2){
+        
---
+++ loopexample.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <?php
+        $count = 0;
+        while ($count < 10){
+            echo "<p>my number in the loop is: "."<br></br>";
+            $count++;
+        }
+    ?>
+</body>
+</html>
