8af47fb70606e5072e538eb7b07f05802a35df66
elmo911 <jacobmwolfe@gmail.com>
2015-09-16 17:17:20 -0500

    foreach array

--- loopexample.php
+++ loopexample.php
@@ -14,9 +14,15 @@
         }
         while($count <= 10);*/
         
-        for($newcount = 0; $newcount < 10; $newcount++){
+        /*for($newcount = 0; $newcount < 10; $newcount++){
             echo "<p>my number in the loop is: ".$newcount."<br></br>";
-        }
+        }*/
+
+$d = array("zero", "one", "two", "cat", "dog");
+foreach($d as $value){
+    echo "<p>My value in the loop is:  ".$value."<br></br></p>";
+}
+
     ?>
 </body>
 </html>
