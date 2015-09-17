3c03ee531d29145bbac19dbfee036021bff851ed
elmo911 <jacobmwolfe@gmail.com>
2015-09-17 12:59:38 -0500

    Metric Functions

--- metricfunctions.php
+++ metricfunctions.php
@@ -1,3 +1,19 @@
 <?php
+    Class MetricFunctions {
 
+        public function FtoC ($F){
+            $C = ($F-32)*5/9;
+            return $C;
+        }
+
+        public function LBtoKG($LB){
+            $KG = $LB/2.2046;
+            return $KG;
+        }
+
+        public function MItoKM($MI){
+            $KM = $MI/0.62137;
+            return $KM;
+        }
+    }
 ?>
