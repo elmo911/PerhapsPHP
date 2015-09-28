5ef859a3a6fb002ab7e0a81948db729661b51205
elmo911 <jacobmwolfe@gmail.com>
2015-09-28 14:23:50 -0500

    exam practice

---
+++ practice.php
@@ -0,0 +1,20 @@
+<?php
+  $count = 0;
+  $ar = array();
+
+  while (count < 3){
+    echo $count;
+    $ar[$count] = $count;
+    $count++;
+  }
+
+  do {
+    echo "Doing ".$count;
+    $count++;
+    $ar[$count] = $count;
+  } while ($count <= 10);
+
+  foreach ($ar as $num) {
+    echo "array: ".$num;
+  }
+ ?>
