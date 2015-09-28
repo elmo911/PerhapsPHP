dfd9ca4c54967f7810f887f1f0722ae19a1cb5db
elmo911 <jacobmwolfe@gmail.com>
2015-09-28 14:26:19 -0500

    inf loop :(

--- practice.php
+++ practice.php
@@ -2,19 +2,19 @@
   $count = 0;
   $ar = array();
 
-  while (count < 3){
+  while ($count < 3){
     echo $count;
     $ar[$count] = $count;
     $count++;
   }
 
   do {
-    echo "Doing ".$count;
+    echo "<br></br>Doing ".$count;
     $count++;
     $ar[$count] = $count;
   } while ($count <= 10);
 
   foreach ($ar as $num) {
-    echo "array: ".$num;
+    echo "<br></br>array: ".$num;
   }
  ?>
