d5e32f55b6fe5c0533b2bf4798f3868fd71840c0
elmo911 <jacobmwolfe@gmail.com>
2015-09-28 14:54:59 -0500

    display POST

--- auth.php
+++ auth.php
@@ -1,9 +1,16 @@
 <?php
   session_start();
   if($_SESSION['submitted'] == 1){
+    $_SESSION['submitted'] = 0;
     echo "session = 1";
+    echo "User: ".$_POST["user"];
+    echo "Password: ".$_POST["pass"];
   }
   else{
+    $_SESSION['submitted'] = 1;
     echo "session = 0";
+    echo "session = 1";
+    echo "User: ".$_POST["user"];
+    echo "Password: ".$_POST["pass"];
   }
 ?>
