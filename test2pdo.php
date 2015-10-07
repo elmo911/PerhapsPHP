7503658af43ba51d079bd9cf3f7f3eaff0aad5b9
elmo911 <jacobmwolfe@gmail.com>
2015-10-07 17:23:11 -0500

    error ts

--- test2pdo.php
+++ test2pdo.php
@@ -1,5 +1,5 @@
 <?php
-include 'conntodb.php';
+require 'conntodb.php';
 
 $sql = "SELECT userid, name, email FROM acsm_0e7ddff7b7d920f.user_tbl"
 
@@ -7,7 +7,7 @@ try {
   $user_tbl = $connDB->prepare($sql);
   $connDB->execute();
 } catch (PDOException $e) {
-  
+  die(var_dump($e));
 }
 
 while($row = $user_tbl->fetch()){
