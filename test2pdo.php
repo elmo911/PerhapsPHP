0dfb2583729a95575bc81770421a5f24664954db
elmo911 <jacobmwolfe@gmail.com>
2015-10-07 17:31:44 -0500

    missed it

--- test2pdo.php
+++ test2pdo.php
@@ -4,7 +4,7 @@ require ('conntodb.php');
 $sql = "SELECT userid, name, email FROM acsm_0e7ddff7b7d920f.user_tbl";
 
 try {
-  $user_tbl = $connDB->prepare($sql);
+  $user_tbl = $conn->prepare($sql);
   $user_tbl->execute();
 } catch (PDOException $e) {
   die(var_dump($e));
