5327974ae0d0f3a6b9830386038a3e729fe50646
elmo911 <jacobmwolfe@gmail.com>
2015-10-14 18:29:50 -0500

    garrisons math

---
+++ domath.php
@@ -0,0 +1,8 @@
+<?php
+include 'garrisonsmath.php';
+
+$garrisonsmath = new GarrisonsMath();
+
+echo $garrisonsmath->mathstuff(5);
+
+ ?>
---
+++ garrisonsmath.php
@@ -0,0 +1,15 @@
+<?php
+
+  /**
+   *
+   */
+  class GarrisonsMath
+  {
+
+    public function mathstuff($x){
+      return $x^2+4$x+5;
+    }
+
+  }
+  
+ ?>
