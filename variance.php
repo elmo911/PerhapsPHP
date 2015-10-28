2acd1119c8c90e4b18793ea1a67613470cb2e55d
elmo911 <jacobmwolfe@gmail.com>
2015-10-28 16:52:00 -0500

    corResult

--- variance.php
+++ variance.php
@@ -1,8 +1,9 @@
 <?php
-  $sample = array(13 , 14, 30, 3, 18, 18, 50, 20);
+  $sample = array(13, 14, 30, 3, 18, 18, 50, 20);
+
   $mean = 0;
-  $result = 0;
   $N = count($sample);
+
   for($i=0; $i < $N; $i++){
     $mean += $sample[$i];
   }
@@ -14,6 +15,8 @@
   }
 
   $result = (1/$N) * $summation;
+  $corResult = (1/($N-1)) * $summation;
 
-  echo $result;
+  echo $result "<br>";
+  echo $corResult;
  ?>
