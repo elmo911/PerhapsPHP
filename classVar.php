35c4ae6a22e626f14b402bcca2ad17c430c0bef9
elmo911 <jacobmwolfe@gmail.com>
2015-10-28 17:07:44 -0500

    classVar

---
+++ classVar.php
@@ -0,0 +1,38 @@
+<?php 
+/**
+ *
+ */
+class classVar
+{
+public function printVar($sample){
+  $sample = array(13, 14, 30, 3, 18, 18, 50, 20);
+
+  $mean = 0;
+  $N = count($sample);
+
+  for($i=0; $i < $N; $i++){
+    $mean += $sample[$i];
+  }
+  $mean = $mean / $N;
+
+  $summation = 0;
+  for($i=0; $i < $N; $i++){
+    $num = ($mean - $sample[$i]);
+    $num = $num * $num;
+    $summation += $num;
+  }
+
+  $result = (1/$N) * $summation;
+  $corResult = (1/($N-1)) * $summation;
+
+  echo "Sample: 13, 14, 30, 3, 18, 18, 50, 20 <br>";
+  echo "Mean X: ".$mean."<br>";
+  echo "N: ".$N."<br>";
+  echo "Result: ".$result."<br>";
+  echo "Result: ".$corResult;
+}
+
+}
+
+
+ ?>
