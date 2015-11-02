a94f0ce9af8eab7d2e90c380009e6fa91456a210
elmo911 <jacobmwolfe@gmail.com>
2015-11-02 11:26:14 -0600

    binary var

--- HW4/variance.php
+++ HW4/variance.php
@@ -1,19 +1,20 @@
 <?php
   include "classVar.php";
   $myVar = new classVar();
-  $sample1 = array(13, 14, 30, 3, 18, 18, 50, 20);
-  $myVar->printVar($sample1);
+  $sample = array(13, 14, 30, 3, 18, 18, 50, 20);
+  $myVar->printVar($sample);
 
-  $sample2 = array(23, 11, 38, 8, 14, 19, 40, 10);
-  $sample3 = array(19, 15, 28, 1, 12, 13, 20, 70);
-  $sample4 = array(33, 24, 20, 6, 28, 28, 52, 13);
+  $sample1 = array(1, 1, 0, 1, 1, 1, 0, 0);
+  $sample2 = array(0, 1, 0, 1, 0, 1, 0, 1);
+  $sample3 = array(1, 0, 1, 1, 0, 1, 0, 0);
+  $sample4 = array(1, 0, 1, 0, 1, 0, 1, 1);
   $population = array($sample1, $sample2, $sample3, $sample4);
 
   echo 'Samples:<br>
-    Sample 1 = 13, 14, 30, 3, 18, 18, 50, 20<br>
-    Sample 2 = 23, 11, 38, 8, 14, 19, 40, 10<br>
-    Sample 3 = 19, 15, 28, 1, 12, 13, 20, 70<br>
-    Sample 4 = 33, 24, 20, 6, 28, 28, 52, 13<br>
+    Sample 1 = 1, 1, 0, 1, 1, 1, 0, 0<br>
+    Sample 2 = 0, 1, 0, 1, 0, 1, 0, 1<br>
+    Sample 3 = 1, 0, 1, 1, 0, 1, 0, 0<br>
+    Sample 4 = 1, 0, 1, 0, 1, 0, 1, 1<br>
   ';
   $myVar->findPopulationSD($population);
  ?>
