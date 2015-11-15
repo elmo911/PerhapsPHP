3fee5e788a8b74cc4540f1ec9c454b7e20cffb6a
elmo911 <jacobmwolfe@gmail.com>
2015-11-15 14:53:07 -0600

    Use right controller

--- HW5/Controllers/DBSession.php
+++ HW5/Controllers/DBSession.php
@@ -1,41 +1,50 @@
 <?php
-include 'conntodb.php';
-Class dbsession {
+include 'HW5/Controllers/DatabaseConnection.php';
+include 'HW5/Models/SessionVar.php';
+Class DBSession {
 
-public $sesName = "";
-public $sessionVar = [];
+public $sessionName = "";
+public $sessionVars;
 
 function __construct($sesName) {
-  $this->sesName = $sesName;
+  $this->sessionName = $sesName;
+  $this->getSessionVar();
 }
 
 public function getSessionVar(){
-  $sessionVar = [];
+  $sessionVars = [];
+
   $sql_select = "SELECT sessionName, varName, varValue, LastUpdate
-  FROM sessionvar";
-  $stmt = $conn->query($sql_select);
+  FROM sessionvar
+  WHERE sessionName = :curSessionName";
+  $stmt = $conn->prepare($sql_select);
+  $stmt->bindParam(':curSessionName', $this->sessionName, PDO::PARAM_STR);
+  $stmt->execute();
   $returnedList = $stmt->fetchAll();
+
   if(count($returnedList)>0){
-    foreach($returnedList as $group) {
-        $curGroup = new Group($group['group_name']);
-        $curGroup->setID($group['groupid']);
-        $curGroup->setType($group['group_type']);
-        $curGroup->setWeb($group['group_web']);
-        $listOfGroups[] = $curGroup;
+    foreach($returnedList as $var) {
+        $sessionvar = new SessionVar($var['sessionName']);
+        $sessionvar->varName = $var['varName'];
+        $sessionvar->varValue = $var['varValue'];
+        $sessionvar->LastUpdate = $var['LastUpdate'];
+        $sessionVars[] = $sessionvar;
     }
   }
-  return $listOfGroups;
+  $this->$sessionVars = $sessionVars;
+  return $sessionVars;
 }
 
-public function insertvar($varName,$varValue){
+public function insertvar($varName, $varValue){
 	date_default_timezone_set('American/Chicago');
-	$sql = "Insert into sessionVar Values('".$this->sesName."','".$varName."','".$varValue."',getdate())";
+	$sql_insert = "Insert into sessionvar Values(:sessionName, :varName, :varValue ,getdate())";
 	try
 	{
-		$dbsesin = $this->openses();
-		$rs_var = $dbsesin->prepare($sql);
-		$rs_var->execute();
-		$dbsesin = null;
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bindParam(':curSessionName', $this->sessionName, PDO::PARAM_STR);
+    $stmt->execute();
+    $this->getSessionVar();
+    echo "<p>Insert Success.</p>";
 	}
 	catch(Exeception $e) {
 		echo "<p>Insert Failed.</p>";
@@ -43,55 +52,67 @@ public function insertvar($varName,$varValue){
 }
 
 public function varValue($varName){
-	$sql = "select varValue from sessionVar where sessionID = '".$this->sesName."' and VarName ='".$varName."'";
-
-	try
-	{
-		$dbsesin = $this->openses();
-		$rs_var = $dbsesin->prepare($sql);
-		$rs_var->execute();
-		$row = $rs_var->fetch();
-		return ($row['varValue']);
-
-	}
-	catch(Exeception $e) {
-		echo "<p>Insert Failed.</p>";
-	}
-
-
+  $valueList = $this->sessionVars;
+  foreach ($valueList as $sessionVar) {
+    if($sessionVar->varName == $varName){
+      return $sessionVar->varValue;
+    }
+  }
+  return null;
 }
 
-public function updateVal($varName,$varVal) {
+public function updateVal($varName, $varVal) {
 	date_default_timezone_set('American/Chicago');
-	$sql = "update sessionVar set varValue='".$varVal."', LastUpdate = getdate()"." where sessionID='".$this->sesName."' and varName = '".$varName."'";
-	try
-	{
-		$dbsesin = $this->openses();
-		$rs_var = $dbsesin->prepare($sql);
-		$rs_var->execute();
-		 echo "<p>Update successful</p>";
 
+  $sql_update = "UPDATE sessionvar
+  SET varValue= :varVal, LastUpdate = getdate()
+  WHERE sessionName= :curSessionName AND varName = :varName";
+
+  try
+	{
+    $stmt = $conn->prepare($sql_update);
+    $stmt->bindParam(':varVal', $varVal, PDO::PARAM_STR);
+    $stmt->bindParam(':curSessionName', $this->sessionName, PDO::PARAM_STR);
+    $stmt->bindParam(':varName', $varName, PDO::PARAM_STR);
+    $stmt->execute();
+    $this->getSessionVar();
+    echo "<p>Update Success.</p>";
 	}
 	catch(Exeception $e) {
-		echo "<p>Insert Failed.</p>";
+		echo "<p>Update Failed.</p>";
 	}
 }
 
 public function deleteVar($varName){
-	$sql = "delete from sessionVar where sessionID = '".$this->sesName."' and varName = '".$varName."'";
+	$sql_delete = "DELETE FROM sessionvar
+  WHERE sessionName = :curSessionName AND varName = :varName";
 
-	try
+  try
 	{
-		$dbsesin = $this->openses();
-		$rs_var = $dbsesin->prepare($sql);
-		$rs_var->execute();
-		echo "<p>Delete successful</p>";
-
+    $stmt = $conn->prepare($sql_delete);
+    $stmt->bindParam(':curSessionName', $this->sessionName, PDO::PARAM_STR);
+    $stmt->bindParam(':varName', $varName, PDO::PARAM_STR);
+    $stmt->execute();
+    $this->getSessionVar();
+    echo "<p>Delete Success.</p>";
 	}
 	catch(Exeception $e) {
-		echo "<p>Insert Failed.</p>";
+		echo "<p>Delete Failed.</p>";
 	}
 }
 
+  public printVars(){
+    $varList = $this->getSessionVar();
+    if(count($varList)>0){
+      foreach($varList as $var) {
+        echo '
+        <p> '.$var->toString().'</p>';
+      }
+    }
+    else {
+      echo '
+      <p>No session variables set</p>';
+    }
+  }
 }
 ?>
