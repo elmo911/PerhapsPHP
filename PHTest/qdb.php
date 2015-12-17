f1cbd5eb8e2f7b951af6355670111a1cd42beec4
elmo911 <jacobmwolfe@gmail.com>
2015-12-17 12:00:49 -0600

    test

---
+++ PHTest/qdb.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "158.69.203.77";
+$username = "remote";
+$password = "puIe2ohji1Hw1oQCqix76EXs4";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+?>
