54e82cb87bb699382f49b040b9b64d7edf87a238
elmo911 <jacobmwolfe@gmail.com>
2015-12-17 15:29:49 -0600

    ?

--- PHTest/MetaCareAPI.php
+++ PHTest/MetaCareAPI.php
@@ -13,7 +13,7 @@ $required = array('email', 'password');
 // Loop over field names, make sure each one exists and is not empty
 $proceed = false;
 if (isset($_POST['email']) && isset($_POST['password'])){
-  $return = QuestionDB::auth($_POST['email'], $_POST['password']);
+  $return = QuestionDB::auth($_POST['email'], $hash);
   if($return["error"]){
     echo "ERROR|".$return["message"];
   }
